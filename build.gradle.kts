e1f037a14b985b1cdcb8d66ecebc23d567822ec9
Nhu Phuc <phucho0237@gmail.com>
2024-08-14 21:00:43 +0700

    Initialize

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+group = "org.phucho0237"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("net.minestom:minestom-snapshots:6c5cd6544e")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/org/phucho0237/Main.java
@@ -0,0 +1,9 @@
+package org.phucho0237;
+
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.instance.InstanceContainer;
+import net.minestom.server.instance.InstanceManager;
+
+public class Main {
+    public static void main(String[] args) {}
+}
