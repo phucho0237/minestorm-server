5fad1aa5f02f2fbcb45ed6286b43f7af99a8afb0
Nhu Phuc <phucho0237@gmail.com>
2024-08-14 21:17:51 +0700

    Fix console error by adding dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("net.minestom:minestom-snapshots:6c5cd6544e")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
 }
 
 tasks.test {
